207ff91f6c167867eb17d10358dea7a3bfad88e3
oliveiratamiris <tamirisocoelho@gmail.com>
2023-07-06 23:29:01 -0300

    finalizado o exercício distâcia do uri

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,10 +10,17 @@ public class Main {
 
         Scanner sc =  new Scanner(System.in);
 
+        int distanciaEmKm , tempoEmMinutos;
 
-        
+        distanciaEmKm = sc.nextInt();
+
+
+        tempoEmMinutos = distanciaEmKm * 2;
+
+
+        System.out.println(tempoEmMinutos + " minutos");
+
         sc.close();
-
 
     }
 }
